22fd7e388f744a047a793bd3dd7d9af4a5125033
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-09-12 20:48:02 +0300

    2. Commit
    Youtube Efe Gorkem Umit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./build/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'poppins':['Poppins'],
+        'roboto' :['Roboto'],
+      }
+    },
   },
   plugins: [],
 }
